4a865cf3a02868276881d19abdbfb8e719609c83
Kazeto Yamamoto <kazeto0513@gmail.com>
2018-04-30 16:47:23 +0900

    Updated.

--- sync.py
+++ sync.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os, subprocess, argparse, re, collections
+from functools import partial
 
 DESCRIPTION = 'Syncs directory in local-machine and remote-servers.'
 
@@ -10,11 +11,23 @@ parser.add_argument('src', action='store', nargs=1, type=str, help='Directory to
 parser.add_argument('dests', action='store', nargs='+', type=str, help='Directories to sync in remote machines.')
 parser.add_argument('--dry-run', action='store_true', help='Activates dry-run option of rsync.')
 parser.add_argument('--coding', action='store', type=str, default='utf-8', help='Coding system of your prompt.')
-args = parser.parse_args()
-
-OPTS = ['-ahvz'] + ['--exclude=%s' % e for e in EXCLUSION]
+opts = parser.parse_args()
 
 is_local = lambda host: (host == 'local')
+
+def ansi_color(code, text, is_bold=False):
+    if is_bold:
+        code = ('1;' + code)
+    return '\033[%sm%s\033[0m' % (code, text)
+
+ansi_red    = partial(ansi_color, '31')
+ansi_green  = partial(ansi_color, '32')
+ansi_yellow = partial(ansi_color, '33')
+ansi_blue   = partial(ansi_color, '34')
+ansi_pink   = partial(ansi_color, '35')
+ansi_cyan   = partial(ansi_color, '36')
+ansi_silver = partial(ansi_color, '37')
+ansi_gray   = partial(ansi_color, '90')
 
 
 ## Target of sync.
@@ -26,18 +39,46 @@ class Target:
     def __str__(self):
         if self.is_local():
             return self.path
-        else
+        else:
             return '%s:%s' % (self.host, self.path)
 
     def is_local(self):
         return self.host == 'local'
 
+    @staticmethod
+    def str2target(s):
+        i = s.find(':')
+        if i >= 0:
+            return Target('local', s)
+        else:
+            return Target(s[:i], s[i+1:])
 
+
+## Executes unix-command given and returns its output.
+## @param cmd Command to execute.
+## @return String returned by the command.
+def run_command(cmd, dry_run = False):
+    global opts
+    
+    print('> ' + cmd)
+    
+    if dry_run:
+        return ''
+    else:
+        out = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout
+        return out.decode(opts.coding)
+
+
+## Lists up files of the name given in the target directory.
+## @param target Target instance.
+## @param name   Snipet for find command.
 def find_files(target, name):
     if target.is_local():
-        return run_command('find %s -name %s' % target.path, name).split()
+        cmd = 'find "%s" -name "%s"' % (target.path, name)
     else:
-        return run_command('ssh %s find %s -name %s' % (target.host, target.path, name)).split()
+        cmd = 'ssh %s find "%s" -name "%s"' % (target.host, target.path, name)
+        
+    return run_command(cmd).split()
     
 
 ## Reads configuration file at given path.
@@ -52,10 +93,13 @@ def read_syncconf(path = '.sync.conf'):
             if line.startswith('#'):
                 continue # Comment lines
             
-            spl = line.split(maxsplit = 1)
+            spl = line.split()
             
             if spl[0] == 'only':
-                set_only.add(tuple(spl[1].split(':', 1)))
+                assert(':' in spl[1])
+                host, snipet = spl[1].split(':', 1)
+                assert(host == 'local' or host == 'remote')
+                set_only.add((host, snipet))
             elif spl[0] == 'ignore':
                 set_ignore.add(spl[1])
 
@@ -65,15 +109,16 @@ def read_syncconf(path = '.sync.conf'):
 
 ## Reads .syncignore files in the directory given and returns snipets to be ignored.
 ## @param target Target of sync.
+## @return Snipets to exclude from sync.
 def read_syncignore(target):
     out = []
     paths = find_files(target, '.syncignore')
 
     for path in paths:
         if target.is_local():
-            ignored = run_command('cat %s' % path).split('\n')
+            ignored = run_command('cat "%s"' % path).split('\n')
         else:
-            ignored = run_command('ssh %s cat %s' % (host, path)).split('\n')
+            ignored = run_command('ssh %s cat "%s"' % (host, path)).split('\n')
             
         reldir = os.path.dirname(path)[len(target.path):].strip('/')
         out += ['%s/%s' % (reldir, x.strip()) for x in ignored]
@@ -81,99 +126,72 @@ def read_syncignore(target):
     return out
 
 
-## Executes unix-command given and returns its output.
-## @param cmd Command to execute.
-def run_command(cmd, coding):
-    print('> ' + cmd)
-    out = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE).stdout
-    return out.decode(coding)
-
-
 ## Removes files which another host should manage.
+## @param target Target instance.
+## @param conf   Return value of read_syncconf().
 def clear(target, conf):
     removed = []
 
     # Enumerate files to remove
     for host, snipet in conf.only:
         if host != target.host:
-            removed += [x.strip() for x in find_files(target, snipet).split('\n')]
+            removed += ['"%s"' % x.strip() for x in find_files(target, snipet).split('\n')]
 
     # Remove files
     if target.is_local():
-        pass
+        run_command(' '.join(['rm'] + removed), opts.dry_run)
     else:
-        pass
+        run_command(' '.join(['ssh', target.host, 'rm'] + removed), opts.dry_run)
+
 
 ## Executes rsync.
 ## @param src Source of sync.
 ## @param dest Destination of sync.
-## @param args Parsing result of command options.
-def rsync(src, dest, args):
+## @param conf Return value of read_syncconf().
+def rsync(src, dest, conf):
+    global opts
     assert(src.is_local() or dest.is_local())
 
-    ignored = read_syncignore(src)
+    ignored = read_syncignore(src) + list(conf.ignore)
+
+    for host, snipet in conf.only:
+        if host == src.host:
+            ignored.append(snipet)
 
     cmd = ' '.join(
-        ['rsync', '-ahvz'] +
-        (['--dry-run'] if args.dry_run else []) +
-        ['--exclude=%s' % e for e in ignored] +
+        ['rsync', '-ahvz', '--update'] +
+        (['--dry-run'] if opts.dry_run else []) +
+        ['--exclude="%s"' % e for e in ignored] +
         [str(src), str(dest)])
 
     run_command(cmd, args.coding)
 
 
-## Executes rsync in update-mode.
-def update(target):
-    opts = OPTS + ['--update']
-
-    for remote in get_remotes():
-        print_job(target, remote, LOCAL, 'update')
-        rsync(target, remote, LOCAL, opts)
-
-    opts += ['--exclude=%s' % e for e in ENCRYPTED]
-
-    for remote in get_remotes():
-        print_job(target, LOCAL, remote, 'update')
-        rsync(target, LOCAL, remote, opts)
-
-
-## Executes rsync in delete-mode.
-def delete(target):
-    opts = OPTS + ['--delete'] + ['--exclude=%s' % e for e in ENCRYPTED]
-
-    for remote in get_remotes():
-        print_job(target, LOCAL, remote, 'delete')
-        rsync(target, LOCAL, remote, opts)
-
-
 ## Main procedure.
 ## Reads command line and executes rsync in the mode specified.
 def main():
-    global is_dry_run, do_target_daiba
+    global opts
 
-    description = '\n'.join([
-        'This script to sync working directories in local and remote-servers.',
-        'Update mode copies all files in all workspaces to all workspaces.',
-        'Delete mode deletes files which do not exist in local workspace.'])
+    conf = read_syncconf('./.sync.conf')
+
+    src = Target.str2target(opts.src)
+    dests = [Target.str2target(d) for d in opts.dests]
+
+    assert(src.is_local())
+    assert(not any(Target.is_local, dests)) # dests are remotes
+
+    # [sync] remotes -> local
+    for dest in dests:
+        rsync(src, dest)
+        
+    # [sync] local -> remotes
+    for dest in dests:
+        rsync(dest, src)
+
+    # remove redundant files
+    for t in [src] + dests:
+        clear(t, conf)
     
-    parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('mode', action='store', nargs=1, type=str, choices=['delete', 'update'], help='Execution mode.')
-    parser.add_argument('--dry-run', action='store_true', help='Activates dry-run option of rsync.')
-    parser.add_argument('--daiba', action='store_true', help='Syncs with remote-servers in daiba cluster.')
-
-    args = parser.parse_args()
-    mode = args.mode[0]
-    is_dry_run = args.dry_run
-    do_target_daiba = args.daiba
-
-    if mode == 'update':
-        for t in TARGETS:
-            update(t)
-
-    if mode == 'delete':
-        for t in TARGETS:
-            delete(t)
-
 
 if __name__=='__main__':
     main()
